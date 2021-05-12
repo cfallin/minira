9eba56b71e64d9a66f297f648195eba2fdcebd93
Chris Fallin <chris@cfallin.org>
2021-05-12 16:56:44 -0700

    parser fixes for I64/V128 world

--- fuzz/fuzz_targets/regalloc2.rs
+++ fuzz/fuzz_targets/regalloc2.rs
@@ -4,28 +4,8 @@ use libfuzzer_sys::fuzz_target;
 use minira::{self, test_framework as ir};
 use regalloc;
 
-// This is the number of test cases the fuzzing framework has given to us so
-// far.  More then half of these get rejected as having unreachable blocks, or
-// critical edges, or live values into the start node, or for whatever reason
-// they are invalid.  Hence ..
-static mut COUNTER_GEN: usize = 0;
-
-// .. this is used to count the number of test cases which actually made it
-// through the allocator.  This number gives a better measure of the extent of
-// test coverage.
-static mut COUNTER_OK: usize = 0;
-
 fuzz_target!(|func: ir::Func| {
     let _ = env_logger::try_init();
-    let n_gen = unsafe {
-        COUNTER_GEN += 1;
-        COUNTER_GEN
-    };
-    let n_ok = unsafe { COUNTER_OK };
-    println!(
-        "==== BEGIN fuzz_regalloc2.rs: #gen'd {:?} #ok {} ========================",
-        n_gen, n_ok
-    );
 
     for inst in func.insns.iter() {
         // We haven't wired up safepoint metadata in the shim yet.
@@ -64,10 +44,6 @@ fuzz_target!(|func: ir::Func| {
     match ra_result {
         Ok(result) => {
             func.update_from_alloc(result);
-            unsafe {
-                COUNTER_OK += 1;
-            }
-            return;
         }
         Err(err) => {
             let mut stop = false;
@@ -81,9 +57,7 @@ fuzz_target!(|func: ir::Func| {
                     .render("==== fuzz_regalloc2.rs: failing input:", &mut rendered)
                     .unwrap();
                 println!("{}", rendered);
-            }
-            println!("==== fuzz_regalloc2.rs: failure reason: {}", err);
-            if stop {
+
                 println!("==== fuzz_regalloc2.rs:");
                 println!(
                     "==== fuzz_regalloc2.rs: to repro, use flags '-f {} -i {}'",
